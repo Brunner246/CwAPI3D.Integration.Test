073f93b8c33fd07b0018714d36596e23b1cf47b1
Michael Brunner <brunner@cadwork.swiss>
2024-04-12 16:41:11 +0200

    - update function logging

--- Logger.cpp
+++ Logger.cpp
@@ -4,6 +4,8 @@
 
 #include "Logger.h"
 
+#include <utility>
+
 std::string getCurrentTime() {
     auto now = std::chrono::system_clock::now();
     auto in_time_t = std::chrono::system_clock::to_time_t(now);
@@ -16,8 +18,8 @@ std::string getCurrentTime() {
 
 class Logger : public LoggerInterface {
 public:
-    explicit Logger(const std::string &fileName, std::ios_base::openmode mode) {
-        file.open(fileName, mode);
+    explicit Logger(std::string fileName, std::ios_base::openmode mode)
+            : fileName(std::move(fileName)), mode(mode) {
     }
 
     Logger(const Logger &other) = delete;
@@ -36,9 +38,13 @@ public:
 
 private:
     std::ofstream file;
+    std::string fileName;
+    std::ios_base::openmode mode;
 
     void do_log(const std::string &message) override {
+        file.open(fileName, mode);
         file << getCurrentTime() << ": " << message << "\n";
+        file.close();
     }
 };
 
@@ -47,7 +53,7 @@ LoggerInterface *instance = nullptr;
 LoggerInterface *getLogger(std::string const &aFileName) {
     static auto init = [fileName = aFileName]() {
         if (!instance)
-            instance = new Logger(fileName, std::ios::out | std::ios::trunc);
+            instance = new Logger(fileName, std::ios::out | std::ios::app);
         return true;
     }();
     return instance;
