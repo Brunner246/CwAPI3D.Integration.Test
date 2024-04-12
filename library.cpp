19dddaebdadd893e63ccf93eb1601fb7522dcdb7
Michael Brunner <brunner@cadwork.swiss>
2024-04-12 13:52:58 +0200

    - reformat

--- library.cpp
+++ library.cpp
@@ -5,16 +5,16 @@
 #include "Logger.h"
 
 
-CWAPI3D_PLUGIN bool plugin_x64_init(CwAPI3D::ControllerFactory* aFactory);
+CWAPI3D_PLUGIN bool plugin_x64_init(CwAPI3D::ControllerFactory *aFactory);
 
-bool plugin_x64_init(CwAPI3D::ControllerFactory* aFactory) {
+bool plugin_x64_init(CwAPI3D::ControllerFactory *aFactory) {
     const std::source_location &lWhere = std::source_location::current();
-  auto lLogger = getLogger();
+    auto lLogger = getLogger();
 
     if (!aFactory) {
         auto lMessage = std::format<std::string>("ControllerFactory is null in file {0}, line {1}, function {2}",
                                                  lWhere.file_name(), lWhere.line(), lWhere.function_name());
-     lLogger->log(lMessage);
+        lLogger->log(lMessage);
         return false;
     }
 
