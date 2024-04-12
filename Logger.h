03655d79f364793854a022907f37087b525b6daa
Michael Brunner <brunner@cadwork.swiss>
2024-04-12 15:33:50 +0200

    - update cpp version

--- Logger.h
+++ Logger.h
@@ -1,79 +1,82 @@
-//
-// Created by MichaelBrunner on 12/04/2024.
-//
+ //
+ // Created by MichaelBrunner on 12/04/2024.
+ //
 
 
-#include <fstream>
-#include <string>
-#include <chrono>
-#include <ctime>
-#include <sstream>
+ #include <fstream>
+ #include <string>
+ #include <chrono>
+ #include <ctime>
+ #include <sstream>
 
-class LoggerInterface {
-public:
-    virtual ~LoggerInterface() = default;
+#include <iostream>
+#include <iomanip>
 
-    void log(const std::string &message) {
-        do_log(message);
-    }
+ class LoggerInterface {
+ public:
+     virtual ~LoggerInterface() = default;
 
-private:
-    virtual void do_log(const std::string &message) = 0;
+     void log(const std::string &message) {
+         do_log(message);
+     }
 
-};
+ private:
+     virtual void do_log(const std::string &message) = 0;
 
-//template<typename... Args>
-//void log(Args... args) {
-//    file << getCurrentTime() << ": ";
-//    ((file << args << " "), ...);
-//    file << "\n";
-//}
+ };
 
-std::string getCurrentTime() {
-    auto now = std::chrono::system_clock::now();
-    auto in_time_t = std::chrono::system_clock::to_time_t(now);
-    std::tm buf{};
-    localtime_s(&buf, &in_time_t);
-    std::stringstream ss;
-    ss << std::put_time(&buf, "%Y-%m-%d %X");
-    return ss.str();
-}
+ //template<typename... Args>
+ //void log(Args... args) {
+ //    file << getCurrentTime() << ": ";
+ //    ((file << args << " "), ...);
+ //    file << "\n";
+ //}
 
-class Logger : public LoggerInterface {
-public:
-    explicit Logger(const std::string &fileName, std::ios_base::openmode mode) {
-        file.open(fileName, mode);
-    }
+ std::string getCurrentTime() {
+     auto now = std::chrono::system_clock::now();
+     auto in_time_t = std::chrono::system_clock::to_time_t(now);
+     std::tm buf{};
+     localtime_s(&buf, &in_time_t);
+     std::stringstream ss;
+     ss << std::put_time(&buf, "%Y-%m-%d %X");
+     return ss.str();
+ }
 
-    Logger(const Logger &other) = delete;
+ class Logger : public LoggerInterface {
+ public:
+     explicit Logger(const std::string &fileName, std::ios_base::openmode mode) {
+         file.open(fileName, mode);
+     }
 
-    Logger &operator=(const Logger &other) = delete;
+     Logger(const Logger &other) = delete;
 
-    Logger(Logger &&other) = delete;
+     Logger &operator=(const Logger &other) = delete;
 
-    Logger &operator=(Logger &&other) = delete;
+     Logger(Logger &&other) = delete;
 
-    explicit operator bool() const { return file.is_open(); }
+     Logger &operator=(Logger &&other) = delete;
 
-    ~Logger() override {
-        if (file.is_open()) { file.close(); }
-    }
+     explicit operator bool() const { return file.is_open(); }
 
-private:
-    std::ofstream file;
+     ~Logger() override {
+         if (file.is_open()) { file.close(); }
+     }
 
-    void do_log(const std::string &message) override {
-        file << getCurrentTime() << ": " << message << "\n";
-    }
-};
+ private:
+     std::ofstream file;
 
-LoggerInterface *instance = nullptr;
+     void do_log(const std::string &message) override {
+         file << getCurrentTime() << ": " << message << "\n";
+     }
+ };
 
-LoggerInterface *getLogger() {
-    static auto init = []() {
-        if (!instance)
-            instance = new Logger("log.txt", std::ios::out | std::ios::trunc);
-        return true;
-    }();
-    return instance;
-}
+ LoggerInterface *instance = nullptr;
+
+ LoggerInterface *getLogger(std::string const& aFileName = "log.txt"){
+     static auto init = [fileName = aFileName]() {
+         if (!instance)
+             instance = new Logger(fileName, std::ios::out | std::ios::trunc);
+         return true;
+     }();
+     return instance;
+ }
